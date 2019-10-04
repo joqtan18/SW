8a254fcea9f03049c51022a067549981d85db943
joqtan18 <joqtan.jr@gmail.com>
2019-10-03 21:28:00 -0500

    imagen

--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>DREC</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -30,10 +30,7 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admisss <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3"> <img src="{{asset('img/logotip.png')}}"> </div>
       </a>
 
       <!-- Divider -->
